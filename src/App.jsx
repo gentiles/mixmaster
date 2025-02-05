77af9e9d2d6a7dd843b853a09827cfbb1abd48a9
erwin <erwinmarin05@gmail.com>
2025-02-06 07:54:01 +0800

    create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h2>Home page</h2>,
+  },
+  {
+    path: "/about",
+    element: (
+      <div>
+        <h2>About page</h2>
+      </div>
+    ),
+  },
+]);
 const App = () => {
   return <h2>mixmaster starter</h2>;
 };
